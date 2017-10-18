afee344903f21f549941e2a1d596f2499302799f
Allen <blue_sky1997@live.com>
2017-10-18 18:33:51 +0800

    Nightly Commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./babel/'))
+app.use(express.static('./babel/'));
 
 app.listen(80, 'localhost', () => {
     console.log(`Server is running on localhost:80`);
