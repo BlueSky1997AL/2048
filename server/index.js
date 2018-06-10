0e40b1eb31d6cf49fee4996640a9b5fe88483b4a
Allen Lawrence <blue_sky1997@live.com>
2018-06-10 13:36:27 +0800

    Changed folder structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./doc/'));
+app.use(express.static('./ES5/'));
 
 app.listen(80, () => {
     console.log(`Server is running on localhost:80`);
