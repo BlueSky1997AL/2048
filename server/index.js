8e3801a73bd65ed62dc035795c09f4f7992548c5
Allen Lawrence <blue_sky1997@live.com>
2018-06-13 17:16:37 +0800

    Revised README.md

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./ES5/'));
+app.use(express.static('./es5/'));
 
-app.listen(80, () => {
-    console.log(`Server is running on localhost:80`);
+app.listen(3000, () => {
+    console.log(`Server is running on localhost:3000`);
 });
